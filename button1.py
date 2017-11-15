9e8eeff6dfd51dfee5ae8e11f57d3d96e273d921
DongHyeon Kang <dbslt11@naver.com>
2017-11-15 16:14:06 +0900

    birthday

--- button1.py
+++ button1.py
@@ -3,9 +3,11 @@ from tkinter import *
 window =Tk()
 
 b1 = Button(window, text="DongHyeon kang")
-b2 = Button(window, text="60162088")
+b2 = Button(window, text="5/27")
+b3 = Button(window, text="60162088")
 
 b1.grid(row=0, column=0)
-b2.grid(row=1, column=1)
+b2.grid(row=1, column=0)
+b3.grid(row=1, column=1)
 
 window.mainloop()
